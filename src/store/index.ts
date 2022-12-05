dbe29f0c882c6e5245cd28cdae388a81dbf71e95
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-12-06 00:16:09 +0100

    genelreducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,18 @@
 import { combineReducers } from "redux" 
 
-const rootReducer = combineReducers
+
+//app leri genelde  boyle yapariy
+interface AppState {
+    user:any;
+    categories:any;
+    records:any;
+}
+
+//burada kullanici  category records yapariz
+//tutacagimiz degiskenleri genel olrak reducer
+const rootReducer = combineReducers<AppState>({
+    user:()=>{},
+    categories:()=>{},
+    records:()=>{}
+})
+export default rootReducer
