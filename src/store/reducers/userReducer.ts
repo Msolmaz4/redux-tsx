c5e331f4d3702b867b0ce7c32bfd7b6b44e0cc3e
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-12-06 22:14:08 +0100

    action

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,8 +1,12 @@
 //burada kullanicidan gelen bilgiler burada isleyecegiy
 //temelde boyle kurdugumuydan hata aliriy sepep satae ve actiom type girilmediginden
 //temel kurulumfdan sonra typede user dosyadsi actik 
+//trpe yaptik burda statet yazariz
+//sonra action geceriz uc asama oliiuur kullanici bilgi alirken start succes error uc asama olir bunu bend etped duyenleriz
 
-const userReducer = (state,action)=>{
+import { UserAction, UserState } from "../../types/user"
+
+const userReducer = (state:UserState,action:UserAction)=>{
     return state
 }
 
--- src/types/user.ts
+++ src/types/user.ts
@@ -5,3 +5,26 @@ export interface User {
     full_name:string;
     token:string;
 }
+export interface UserState {
+    data:User;
+    loading:boolean;
+    error:string;    
+}
+
+//login olurken dispatch olusturmak
+interface LOGIN_START {
+    type:'LOGIN_START'
+
+}
+interface LOGIN_SUCCESS {
+    type:'LOGIN_SUCCES';
+    payload:User;
+
+}
+interface LOGIN_ERROR {
+    type:'LOGIN_ERROR';
+
+}
+
+//UCUNU BIRLESTIRMEK GEREKIR
+export type UserAction = LOGIN_START | LOGIN_SUCCESS | LOGIN_ERROR
