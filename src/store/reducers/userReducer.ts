e376fcade30dab3a1dc8b2634ee1cb11c4c32147
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-12-06 22:01:34 +0100

    usertype

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,5 +1,6 @@
 //burada kullanicidan gelen bilgiler burada isleyecegiy
 //temelde boyle kurdugumuydan hata aliriy sepep satae ve actiom type girilmediginden
+//temel kurulumfdan sonra typede user dosyadsi actik 
 
 const userReducer = (state,action)=>{
     return state
---
+++ src/types/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+    message:string;
+    username:string;
+    email:string;
+    full_name:string;
+    token:string;
+}
